94cd6588ffa0ccb17c888a101cd6233e6f03f1ca
Ambrose47 <haconghieu2610@gmail.com>
2023-10-14 16:58:22 +0700

    update recursion

--- src/main/java/com/haconghieu/mathutil/core/MathUtility.java
+++ src/main/java/com/haconghieu/mathutil/core/MathUtility.java
@@ -17,8 +17,23 @@ public class MathUtility {
     //hàm tính n! = 1.2.3....n
     //n<0 n>20 không tính do 21! tràn kiểu long
     // 0! =1! =1
-    public static long getFactorial(int n){
-        long result = 1;
+//    public static long getFactorial(int n){
+//        long result = 1;
+//        
+//        if(n < 0|| n>20){
+//            throw new IllegalArgumentException("Invalid");           
+//        }
+//        if(n == 0 || n == 1){
+//            return 1;
+//        }
+//        for (int i = 2; i <= n; i++) {
+//            result *= i;
+//        }
+//        return result;
+//    }
+    //  NÂNG CẤP HÀM TÍNH GIAI THỪA DÙNG THUẬT TOÁN ĐỆ QUY 
+    // ĐỆ QUY LÀ GỌI LẠI CHÍNH MÌNH VỚI QUY MÔ NHỎ HƠN (ĐỆ QUY -RECURSION)
+        public static long getFactorial(int n){
         
         if(n < 0|| n>20){
             throw new IllegalArgumentException("Invalid");           
@@ -26,9 +41,9 @@ public class MathUtility {
         if(n == 0 || n == 1){
             return 1;
         }
-        for (int i = 2; i <= n; i++) {
-            result *= i;
-        }
-        return result;
+        return n * getFactorial(n-1);
     }
+        
+// KĨ THUẬT ĐÃ XANH, TEST LẠI ĐỂ VẪN XANH, MÀ KHÔNG CẦN NHÌN CHI TIẾT CON
+// NGƯỜI TA GỌI LÀ : REGRESSTION TEST - TEST HỒI QUY
 }
